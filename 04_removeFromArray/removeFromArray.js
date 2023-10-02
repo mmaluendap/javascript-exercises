4d38feda938048aadd67a4e99372f8bc81b9aca7
Martin Maluenda <48397267+mmaluendap@users.noreply.github.com>
2023-10-02 00:13:13 -0300

    feat: 04_removeFromArray passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+  let argsToRemove = [];
+  for (let i = 1; i < arguments.length; i++) {
+    argsToRemove.push(arguments[i]);
+  }
+  return array.filter((element) => !argsToRemove.includes(element));
 };
 
 // Do not edit below this line
