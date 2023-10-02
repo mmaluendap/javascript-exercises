6ed1479bbe3d5d8aa99f9f06448f1afe114713f8
Martin Maluenda <48397267+mmaluendap@users.noreply.github.com>
2023-10-01 23:57:17 -0300

    feat: 02_repeatString passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  let results = "";
+  for (let i = 0; i < num; i++) {
+    results += string;
+  }
+  return results;
 };
 
 // Do not edit below this line
