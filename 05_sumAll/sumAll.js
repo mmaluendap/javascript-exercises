87abe9d24a30d9538ea6b01e4831ac77c2f7b654
Martin Maluenda <48397267+mmaluendap@users.noreply.github.com>
2023-10-02 23:23:38 -0300

    feat: 05_sumAll passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (typeof num1 != "number" || typeof num2 != "number") return "ERROR";
+  let tot = 0;
+  const min = Math.min(num1, num2);
+  const max = Math.max(num1, num2);
+  for (let i = min; i < max + 1; i++) {
+    tot += i;
+  }
+  return tot;
 };
 
 // Do not edit below this line
